a70c4b2e3706de3694bf7021795a95541884b24e
Ben Iofel <iofelben@gmail.com>
2019-02-24 23:18:50 -0500

    move left and right

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,27 @@
 #include <FastLED.h>
 
 #define N_LEDS 16
-#define INITIAL_LENGTH 4
-#define INITIAL_P1 2
-#define INITIAL_P2 8
 
 #define EMPTY 0
-#define P1 1
-#define P2 2
 
-int board[N_LEDS];
+#define P1_RIGHT_BUT 8
+#define P1_LEFT_BUT 9
+#define P1_PUNCH_BUT 10
+#define P0_RIGHT_BUT 11
+#define P0_LEFT_BUT 12
+#define P0_PUNCH_BUT 13
+
 CRGB leds[N_LEDS];
 
 typedef struct {
     int pos;
     int len;
+    unsigned long last_move;
 } player;
 
 player players[2] = {
-    { .pos = 2, .len = 5 },
-    { .pos = 8, .len = 5 },
+    { .pos = 2, .len = 4, .last_move = 0 },
+    { .pos = 8, .len = 4, .last_move = 0 },
 };
 
 void flush() {
@@ -28,9 +30,9 @@ void flush() {
         leds[i] = CRGB::Black;
 
     for (int j = players[0].pos; j < players[0].pos + players[0].len; ++j)
-        leds[j % N_LEDS] = CRGB(1,0,0);
+        leds[j % N_LEDS] += CRGB(1,0,0);
     for (int j = players[1].pos; j < players[1].pos + players[1].len; ++j)
-        leds[j % N_LEDS] = CRGB(0,0,1);
+        leds[j % N_LEDS] += CRGB(0,0,1);
 
     FastLED.show();
 }
@@ -46,7 +48,7 @@ void moveLeft(int playerNum) {
 
 void moveRight(int playerNum) {
     player& p = players[playerNum];
-    if (p.pos + p.len == N_LEDS) { // wrap
+    if (p.pos == N_LEDS - 1) { // wrap
         p.pos = 0;
     } else {
         p.pos++;
@@ -54,22 +56,58 @@ void moveRight(int playerNum) {
 }
 
 void setup() {
+    pinMode(P0_LEFT_BUT, INPUT_PULLUP);
+    pinMode(P0_RIGHT_BUT, INPUT_PULLUP);
+    pinMode(P0_PUNCH_BUT, INPUT_PULLUP);
+    pinMode(P1_LEFT_BUT, INPUT_PULLUP);
+    pinMode(P1_RIGHT_BUT, INPUT_PULLUP);
+    pinMode(P1_PUNCH_BUT, INPUT_PULLUP);
+
     Serial.begin(9600);
 
     FastLED.addLeds<NEOPIXEL, 7>(leds, N_LEDS);
-
-    for (int i = 0; i < N_LEDS; ++i)
-        board[i] = EMPTY;
-
-    for (int i = 0; i < 2; ++i)
-        for (int j = players[i].pos; j < players[i].pos + players[i].len; ++j)
-            board[j] = i;
 
     flush();
 }
 
 void loop() {
-    moveLeft(1);
+    if (millis() - players[0].last_move > 300) {
+        if (digitalRead(P0_LEFT_BUT) == LOW) {
+            moveLeft(0);
+            Serial.println("P0 left");
+            players[0].last_move = millis();
+        }
+
+        if (digitalRead(P0_RIGHT_BUT) == LOW) {
+            moveRight(0);
+            Serial.println("P0 right");
+            players[0].last_move = millis();
+        }
+    }
+
+    if (millis() - players[1].last_move > 300) {
+        if (digitalRead(P1_LEFT_BUT) == LOW) {
+            moveLeft(1);
+            Serial.println("P1 left");
+            players[1].last_move = millis();
+        }
+
+        if (digitalRead(P1_RIGHT_BUT) == LOW) {
+            moveRight(1);
+            Serial.println("P1 right");
+            players[1].last_move = millis();
+        }
+    }
+
     flush();
-    delay(500);
+
+    Serial.print("p0: at ");
+    Serial.print(players[0].pos);
+    Serial.print(" len ");
+    Serial.print(players[0].len);
+    Serial.print(", p1: at ");
+    Serial.print(players[1].pos);
+    Serial.print(" len ");
+    Serial.print(players[1].len);
+    Serial.println();
 }
